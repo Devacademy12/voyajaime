005f71fd540c0b613e292f783e5308af8cdb9ad7
Nahla <nahlaabri2@gmail.com>
2026-03-23 14:57:26 +0100

    modification

--- app/lib/sanitize.ts
+++ app/lib/sanitize.ts
@@ -5,6 +5,11 @@ import createDOMPurify from "dompurify";
 export function sanitizeText(value: string) {
   if (!value) return "";
 
+  // ✅ protège contre SSR
+  if (typeof window === "undefined") {
+    return value; // ou ""
+  }
+
   const DOMPurify = createDOMPurify(window);
 
   return DOMPurify.sanitize(value, {
